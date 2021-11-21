82f49d7375423a9bc86c1caaf016a913f90fb7d9
Denis <chepel.denis96@gmail.com>
2021-11-22 00:38:02 +0200

    Add cryptocurrency rate

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function (){
 @@include('_burger.js')
 
+@@include('_crypto_rate.js')
+
 /*draggable false*/
 const imgs = document.getElementsByTagName('img');
     for(let i = 0; i < imgs.length; i++ ) {
