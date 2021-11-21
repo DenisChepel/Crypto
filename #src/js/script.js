c6999ea76956bf47c84f945402aeb8074ed26774
Denis <chepel.denis96@gmail.com>
2021-11-21 23:57:39 +0200

    Add first screen(adaptive)

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,5 +1,11 @@
 window.addEventListener('load', function (){
 @@include('_burger.js')
 
+/*draggable false*/
+const imgs = document.getElementsByTagName('img');
+    for(let i = 0; i < imgs.length; i++ ) {
+        imgs[i].setAttribute("ondragstart", "return false")
+    }
+/*draggable false*/
 })
 
