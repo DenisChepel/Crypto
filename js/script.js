686daf17be73cae4880a42d914365d2ddab4365c
Denis <chepel.denis96@gmail.com>
2021-11-27 02:38:40 +0200

    Added pricing block and form block(with adaptive)

--- js/script.js
+++ js/script.js
@@ -773,11 +773,215 @@ new SocketStreamer({
 
 
 
-/*draggable false*/
+    /*draggable false*/
 const imgs = document.getElementsByTagName('img');
     for(let i = 0; i < imgs.length; i++ ) {
         imgs[i].setAttribute("ondragstart", "return false")
     }
-/*draggable false*/
+    /*draggable false*/
+
+
+    /* checked card */
+
+    let cardBasic = document.querySelector('#p10')
+    let cardPremium = document.querySelector('#p20')
+    let cardUnlimited = document.querySelector('#p30')
+    let checkBasic = document.querySelector('.check_circle_basic')
+    let checkPremium = document.querySelectorAll('.check_circle_premium')
+    let checkUnlimited = document.querySelectorAll('.check_circle_unlimited')
+
+    cardBasic.addEventListener('mouseenter', function (){
+        this.classList.add('p_card_active')
+        checkBasic.style.fill = '#FFA825'
+        checkPremium.forEach(premiumCircle => {
+            premiumCircle.style.fill = '#ffffff'
+        })
+        checkUnlimited.forEach(unlimitedCircle => {
+            unlimitedCircle.style.fill = '#ffffff'
+        })
+        cardPremium.classList.remove('p_card_active')
+        cardUnlimited.classList.remove('p_card_active')
+    })
+    cardPremium.addEventListener('mouseenter', function (){
+        this.classList.add('p_card_active')
+        checkBasic.style.fill = '#ffffff'
+        checkPremium.forEach(premiumCircle => {
+            premiumCircle.style.fill = '#FFA825'
+        })
+        checkUnlimited.forEach(unlimitedCircle => {
+            unlimitedCircle.style.fill = '#ffffff'
+        })
+        cardBasic.classList.remove('p_card_active')
+        cardUnlimited.classList.remove('p_card_active')
+    })
+    cardUnlimited.addEventListener('mouseenter', function (){
+        this.classList.add('p_card_active')
+        checkBasic.style.fill = '#ffffff'
+        checkPremium.forEach(premiumCircle => {
+            premiumCircle.style.fill = '#ffffff'
+        })
+        checkUnlimited.forEach(unlimitedCircle => {
+            unlimitedCircle.style.fill = '#FFA825'
+        })
+        cardBasic.classList.remove('p_card_active')
+        cardPremium.classList.remove('p_card_active')
+    })
+
+    /* checked card end */
+
+    /*form validate*/
+    /*form validate*/
+
+$(function(){
+    $.validator.setDefaults({
+        errorClass: 'help-msg',
+        highlight: function (e){
+            $(e)
+             .closest('.int')
+             .addClass('invalid')
+             .removeClass('valid')
+        },
+        unhighlight: function (e){
+            $(e)
+            .closest('.int')
+            .removeClass('invalid')
+            .addClass('valid')
+        }
+    })
+
+    $.validator.addMethod("email",function(value,element) {
+        if(this.optional(element))
+        {
+          return true;
+        }else if(/^[A-Za-z0-9._%+--/|{}?]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(value))
+        {
+          return true;
+        }
+        return false;
+    },"Incorrect email");
+
+    $.validator.addMethod("password",function(value,element) {
+        if(this.optional(element))
+        {
+         return true;
+        } else if(/^(?=.*\d)(?=.*[A-Z]).{6,}$/.test(value))
+        {
+         return true;
+        }
+        return false;
+    },"The password must be at least 6 characters long and contain at least one number and an upper and lower case letter of the Latin alphabet");
+
+    $(".form_main").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            l_name: {
+                required: true,
+                minlength: 2
+
+            },
+            password: {
+                password: true,
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+            checkbox: {
+                required: true
+            }
+        },
+        messages: {
+            name: {
+                required: "This field is required"
+            },
+            l_name: {
+                required: "This field is required"
+            },
+            password: {
+                required: "This field is required"
+            },
+            email: {
+                required: "This field is required"
+            },
+            checkbox: {
+                required: ""
+            },
+        }
+    });
+    $.validator.setDefaults({
+        errorClass: 'help-msg',
+        highlight: function (e){
+            $(e)
+                .closest('.int_back')
+                .addClass('invalid')
+                .removeClass('valid')
+        },
+        unhighlight: function (e){
+            $(e)
+                .closest('.int_back')
+                .removeClass('invalid')
+                .addClass('valid')
+        }
+    })
+    $(".form_back").validate({
+        rules: {
+            password: {
+                password: true,
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+        },
+        messages: {
+            password: {
+                required: "This field is required"
+            },
+            email: {
+                required: "This field is required"
+            },
+        }
+    });
+})
+
+
+/*form validate end*/
+
+    /*form validate end*/
+
+    /*form switch*/
+    let formFront = document.querySelector('.form_wrap_front')
+    let formBack = document.querySelector('.form_wrap_back')
+    let itemSwitch = document.querySelector('#reg-log')
+
+    itemSwitch.addEventListener('click', function (){
+        if(this.checked){
+            formFront.style.display = "none"
+            formBack.style.display = "block"
+        } else {
+            formFront.style.display = "block"
+            formBack.style.display = "none"
+        }
+    })
+    /*form switch end*/
+
+    /*scroll to form*/
+    let btnToForm = document.querySelectorAll('.to_form')
+
+    btnToForm.forEach(scrollTo => {
+        scrollTo.addEventListener('click', function (){
+            document.getElementById('form').scrollIntoView( {
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    })
+
+    /*scroll to form end*/
 })
 
